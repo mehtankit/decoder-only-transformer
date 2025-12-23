67dc272a818fd038dbd18f6b8ad89e48402ae83e
mehtankit <ankit75me@gmail.com>
2025-12-23 14:04:46 +0530

    Created tokenization and embeddings module

---
+++ src/tokenization_embeddings.py
@@ -0,0 +1,45 @@
+import tiktoken
+import torch
+import re
+from torch.utils.data import Dataset, DataLoader
+
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s  for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [token if token in self.str_to_int else "<|unk|>" for token in preprocessed]
+        ids = [self.str_to_int[token] for token in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.:;?_!"()\'])', r'\1', text)
+        return text
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_seq = token_ids[i:i+max_length]
+            target_seq = token_ids[i+1:i+max_length+1]
+            self.input_ids.append(torch.tensor(input_seq, dtype=torch.long))
+            self.target_ids.append(torch.tensor(target_seq, dtype=torch.long))
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, num_workers=0,drop_last=True):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=True) 
+    return dataloader
